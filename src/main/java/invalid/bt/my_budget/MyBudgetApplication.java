bbad6eee17f5bf73b25c04916613b7f1322d3438
Benjamin <benjamin.guy.thomas@gmail.com>
2022-07-07 21:01:19 +0200

    Rename

--- src/main/java/invalid/bt/my_budget/MyBudgetApplication.java
+++ src/main/java/invalid/bt/my_budget/MyBudgetApplication.java
@@ -1,4 +1,4 @@
-package invalid.bt.my_budget_sb;
+package invalid.bt.my_budget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/invalid/bt/my_budget/MyBudgetApplicationTests.java
+++ src/test/java/invalid/bt/my_budget/MyBudgetApplicationTests.java
@@ -1,4 +1,4 @@
-package invalid.bt.my_budget_sb;
+package invalid.bt.my_budget;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
