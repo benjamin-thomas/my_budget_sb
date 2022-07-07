a0c53923a3029ba1fccb80653010ffd80a2f9d27
Benjamin <benjamin.guy.thomas@gmail.com>
2022-07-07 20:51:55 +0200

    Init

---
+++ src/main/java/invalid/bt/my_budget_sb/MyBudgetApplication.java
@@ -0,0 +1,13 @@
+package invalid.bt.my_budget_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBudgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/invalid/bt/my_budget_sb/MyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package invalid.bt.my_budget_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
