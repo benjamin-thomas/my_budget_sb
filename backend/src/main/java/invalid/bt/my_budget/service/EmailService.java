cdb0f6d99ca15cdd0c2773d36c0998fc6ca39c0e
Benjamin <benjamin.guy.thomas@gmail.com>
2022-07-19 12:39:03 +0200

    Finalize on choosing JobRunr...
    ...I looked into the much more complicated Quartz package. It looks over-engineered to me, and not free of nasty bugs, especially in multi-node setup.
    
    For this reason, I'm keeping with this nice and simple package.

--- backend/src/main/java/invalid/bt/my_budget/service/EmailService.java
+++ backend/src/main/java/invalid/bt/my_budget/service/EmailService.java
@@ -1,9 +1,13 @@
 package invalid.bt.my_budget.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jobrunr.jobs.annotations.Job;
+import org.jobrunr.spring.annotations.Recurring;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailService {
 
     record SendParams(String recipient, String subject, String body) {
@@ -11,6 +15,17 @@ public class EmailService {
 
     @Job(name = "Send the email address verification token")
     public void send(SendParams sendParams) {
-        System.out.println("--> recipient = " + sendParams.recipient() + ", subject = " + sendParams.subject() + ", body = " + sendParams.body());
+        log.info("--> recipient = " + sendParams.recipient() + ", subject = " + sendParams.subject() + ", body = " + sendParams.body());
+    }
+
+    /**
+     * `@Recurring` is an alternative to Spring's @Scheduled mechanism.
+     * Featuring a stored db entry + the dashboard to monitor the process out of the box.
+     * For this reason, I'll favor this approach, rather than use Spring's builtin mechanism.
+     */
+    @Job(name = "Remove unprocessed signup token after 24h")
+    @Recurring(id = "cleanup-emails", interval = "PT24H")
+    public void removeUnprocessedTokens() {
+        log.info("TODO");
     }
 }
